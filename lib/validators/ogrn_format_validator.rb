d9602ad40c9c7e1605239bfe9fccbcd9a3bd3dcc
Alex Antonov <kaburbundokel11g@inbox.ru>
2014-07-17 16:21:11 +0400

    Refactoring OgrnFormatValidator

--- lib/validators/ogrn_format_validator.rb
+++ lib/validators/ogrn_format_validator.rb
@@ -1,11 +1,7 @@
 class OgrnFormatValidator < ValidatesRussian::Validator
   validates_using do |ogrn|
     next false unless ogrn =~ /^\d+$/
-
-    case ogrn.size
-    when 13 then next false unless (ogrn[0..-2].to_i % 11 % 10) == ogrn[-1].to_i
-    when 15 then next false unless (ogrn[0..-2].to_i % 13 % 10) == ogrn[-1].to_i
-    else next false
-    end
+    next false unless [13, 15].include?(ogrn.size)
+    next false unless (ogrn[0..-2].to_i % (ogrn.size - 2) % 10) == ogrn[-1].to_i
   end
 end
