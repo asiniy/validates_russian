14f2cf848131f2e88211d25ca1a35330f3c9bd26
Andrew Ageev <ageev86@gmail.com>
2016-09-05 16:28:48 +1000

    Исправлены проверки для КПП

--- lib/validators/kpp_format_validator.rb
+++ lib/validators/kpp_format_validator.rb
@@ -3,7 +3,6 @@ class KppFormatValidator < ValidatesRussian::Validator
   validates_using do |kpp|
     next false unless kpp.size == 9
     next false unless ValidatesRussian::REGION_NUMBERS.include?(kpp[0..1])
-    next false unless kpp =~ /^\d+$/
-    next false unless kpp[5..6] != '00'
+    next false unless kpp =~ /^\d{4}([a-z]|\d){2}\d{3}$/i
   end
 end
