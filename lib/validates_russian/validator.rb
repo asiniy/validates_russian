002a2ddbc3c33777dfecd7fe7ec3b603504a2914
Sergei Tikhonov <eni.basic@gmail.com>
2021-11-23 21:47:54 +0500

    fix: wrong number arguments

--- lib/validates_russian/validator.rb
+++ lib/validates_russian/validator.rb
@@ -3,7 +3,7 @@ module ValidatesRussian
     def self.validates_using(&block)
       define_method :validate_each do |record, attribute, value|
         if block[value.to_s] == false
-          record.errors.add(attribute, attribute.to_sym, options.merge(value: value))
+          record.errors.add(attribute, attribute.to_sym, **options.merge(value: value))
         end
       end
     end
